d946e70d1faaf371b85bf38b48c34fc7e6e73ac6
Jedidiah Koh <jedidiahkkh@gmail.com>
2021-08-18 10:43:18 +0800

    Revert "fix: linting"
    This reverts commit d4dec9cdea4ad218dfea8a094cb06b65a3d4fdad.

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,10 +3,14 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: ["airbnb-base", "prettier"],
+  extends: [
+    'airbnb-base',
+    'prettier',
+  ],
   parserOptions: {
     ecmaVersion: 12,
-    sourceType: "module",
+    sourceType: 'module',
   },
-  rules: {},
+  rules: {
+  },
 };
