3bbae77f9caee326c1a5ab511c732d1b8301939c
Sze Ying <ting.szeying@gmail.com>
2021-09-30 01:09:30 +0800

    fix: wrong action name for updating notion property (#14)
    * fix: wrong action name for updating notion property
    
    * style: fix lint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,10 +1,10 @@
 module.exports = {
-    env: {
-        es2021: true,
-    },
-    extends: ["airbnb-base", "prettier"],
-    parserOptions: {
-        ecmaVersion: 12,
-    },
-    rules: {},
+  env: {
+    es2021: true,
+  },
+  extends: ["airbnb-base", "prettier"],
+  parserOptions: {
+    ecmaVersion: 12,
+  },
+  rules: {},
 };
